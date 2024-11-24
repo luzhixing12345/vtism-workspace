4fba40874c737d3035d31ebded012c86052122ec
luzhixing12345 <luzhixing12345@163.com>
2024-11-24 11:32:03 -0500

    [dev] : add run redis script

---
+++ scripts/run_redis.sh
@@ -0,0 +1,29 @@
+
+current_dir=`pwd`
+source ${current_dir}/scripts/check_env.sh
+source ${current_dir}/scripts/kernel_config.sh
+
+run_redis() {
+    redis_path=${current_dir}/benchmark/redis-7.4.0
+    redis_server_exe=${redis_path}/src/redis-server
+    redis_server_args=${redis_path}/redis.conf
+
+    ycsb_exe=${current_dir}/benchmark/ycsb-0.17.0/bin/ycsb
+    workload_path=${redis_path}/workloada.large
+    ycsb_args="load redis -s -P $workload_path -threads 10 -p redis.host=localhost -p redis.port=6379 -p redis.timeout=3600000"
+
+    # run redis server in backend
+    echo "start redis server in backend"
+    ${redis_server_exe} ${redis_server_args} &
+
+    # should wait until loading is done
+    # or cause error: LOADING Redis is loading the dataset in memory
+    # check_redis_loading
+    sleep 10
+
+    run ${ycsb_exe} ${ycsb_args}
+    
+    kill -9 `pgrep redis`
+}
+
+run_redis
