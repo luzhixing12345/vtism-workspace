0b70db6847d618083b1df24647f4b6f5f42794ed
luzhixing12345 <luzhixing12345@163.com>
2025-03-23 19:31:06 -0400

    [dev] : Add kernel config sourcing and e2me notifications in run scripts

--- scripts/run/all.sh
+++ scripts/run/all.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 current_dir=`pwd`
+source ${current_dir}/scripts/kernel_config.sh
 
 echo "---- run all ----"
 # ./${current_dir}/scripts/btree.sh
 ./${current_dir}/scripts/redis.sh
 ./${current_dir}/scripts/graph500.sh
 # ./${current_dir}/scripts/gups.sh
+e2me run --subject "finished 2/5 of $KERNEL_VERSION"
 ./${current_dir}/scripts/pr.sh
 ./${current_dir}/scripts/bc.sh
 ./${current_dir}/scripts/xsbench.sh
+e2me run --subject "finished all of $KERNEL_VERSION"
