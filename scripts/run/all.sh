1746cd48bc0d5b66ca5d7702199f470984319a00
luzhixing12345 <luzhixing12345@163.com>
2025-03-23 19:33:20 -0400

    [dev] : Update script paths for redis, graph500, pr, bc, xsbench

--- scripts/run/all.sh
+++ scripts/run/all.sh
@@ -5,11 +5,11 @@ source ${current_dir}/scripts/kernel_config.sh
 
 echo "---- run all ----"
 # ./${current_dir}/scripts/btree.sh
-./${current_dir}/scripts/redis.sh
-./${current_dir}/scripts/graph500.sh
+./${current_dir}/scripts/run/redis.sh
+./${current_dir}/scripts/run/graph500.sh
 # ./${current_dir}/scripts/gups.sh
 e2me run --subject "finished 2/5 of $KERNEL_VERSION"
-./${current_dir}/scripts/pr.sh
-./${current_dir}/scripts/bc.sh
-./${current_dir}/scripts/xsbench.sh
+./${current_dir}/scripts/run/pr.sh
+./${current_dir}/scripts/run/bc.sh
+./${current_dir}/scripts/run/xsbench.sh
 e2me run --subject "finished all of $KERNEL_VERSION"
