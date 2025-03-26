16100219278ebff946363d1d6e6226737d7a233b
luzhixing12345 <luzhixing12345@163.com>
2025-03-26 12:21:12 -0400

    [dev]  : Adjust btree and gups args for performance

--- scripts/run/btree.sh
+++ scripts/run/btree.sh
@@ -9,7 +9,7 @@ btree_exe=${current_dir}/benchmark/btree/btree
 
 # -n 1<<28
 n=268435456
-btree_args="-- -n $n -l 50000000"
+btree_args="-- -n $n -l 5000000000"
 run ${btree_exe} ${btree_args}
 
 # RSS: 16GB
